a6b6e56a7f952d07503dd0cb24dc1a422d67bd40
Rahma <rahma3a.ismail@gmail.com>
2024-11-10 23:44:52 -0800

    Note.py

---
+++ a3/clinic/note.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+class Note:
+
+    def __init__(self, code, text):
+        self.code = code
+        self.text = text
+        self.timestamp = datetime.now()
+
+    def __eq__(self, other):
+        if isinstance(other, Note):
+            return self.code == other.code and self.text == other.text
+        return False
+
+    def __str__(self):
+        return f"Note({self.code}: {self.text})"
